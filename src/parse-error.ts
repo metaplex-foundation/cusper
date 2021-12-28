a70e4ae3f1e2565358c99be5334f449a91e8c277
Thorsten Lorenz <thlorenz@gmx.de>
2021-12-28 12:13:26 -0500

    feat: resolving/throwing errors and included anchor errors

---
+++ src/parse-error.ts
@@ -0,0 +1,13 @@
+const errorLineRx = /Custom program error: (0x[a-f0-9]+)/i;
+
+export function errorCodeFromLogs(logs: string[]) {
+  for (const line of logs) {
+    const match = line.match(errorLineRx);
+    if (match == null) continue;
+    const hexCode = match[1];
+    try {
+      return parseInt(hexCode);
+    } catch (_: any) {}
+  }
+  return null;
+}
